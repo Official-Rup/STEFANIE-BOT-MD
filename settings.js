c3c01364cb472f3efa22ba24d8b4d9f6c440f968
Official-Rup <107984412+Official-Rup@users.noreply.github.com>
2022-07-15 05:40:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,3 +9,295 @@
 
 
 const _0x434390=_0x4080;function _0x2e24(){const _0x1a4466=['ntilinktg','This\x20Feature\x20Is\x20Only\x20For\x20Bot','cache','The\x20nsfw\x20feature\x20has\x20not\x20been\x20activated,\x20please\x20contact\x20the\x20admin\x20to\x20activate','ntnsfw','nttoxic','ntilinktwt','ntilinkytch','Mr.White\x20Shadow','fluming','doc4','Stefanie\x20Bot\x20Stickers\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a🦄🦄🦄\x20','APIs','sessionName','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','Done\x20✓','wlcm','autoTyping','38084mrvAyA','https://youtube.com/channel/UCKW8EUxAo6A7RKhc35H54wg','Infinity','ntilinktt','session','rpg','flaming','ytname','ntvirtex','Bot\x20Must\x20Be\x20Admin\x20First!','https://zenzapis.xyz','themeemoji','94779529221','34Qweyvh','log0','autoRecord','log','India,\x20Mizoram,\x20Aizawl','ntilinkytvid','vidmenu','botname','flasmurf','ntilink','5862FIReGF','application/zip','redBright','APIKeys','chalk','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','antitags','8DYfCjO','application/vnd.android.package-archive','4566375QGynNM','GitHub:\x20DGXeon','ntilinkig','ownername','watchFile','./XeonMedia/theme/Cheems-bot.mp4','doc5','doc1','261nlxkum','thum','flarun','doc3','limitawal','available','ntwame','Stefanie\x20WhatsApp\x20Bot-MD','err4r','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','Where\x20is\x20the\x20link?','thumb','application/pdf','Error!!','ntilinkall','This\x20Feature\x20Is\x20Only\x20For\x20Admin!','8658900lMNwXc','websitex','1104663iaATQh','owner','./XeonMedia/theme/cheemspic.jpg','resolve','Your\x20Daily\x20Limit\x20Has\x20Expired,\x20The\x20Limit\x20Will\x20Be\x20Reset\x20Every\x2012\x20Hours','author','location','ownertag','readFileSync','vcardowner','autoreadpmngc','application/vnd.openxmlformats-officedocument.wordprocessingml.document','Features\x20Used\x20Only\x20For\x20Private\x20Chat!','15874540vojwqE','your\x20key','2469510LKucCA','unwatchFile','autoReadGc','YT:\x20Xeon','This\x20Feature\x20Is\x20Only\x20For\x20Owner!','doc6','In\x20process...','Feature\x20Used\x20Only\x20For\x20Groups!','socialm'];_0x2e24=function(){return _0x1a4466;};return _0x2e24();}(function(_0x177856,_0x3da3df){const _0x153761=_0x4080,_0x9c066a=_0x177856();while(!![]){try{const _0x2d724c=-parseInt(_0x153761(0x1c2))/0x1*(-parseInt(_0x153761(0x1b8))/0x2)+-parseInt(_0x153761(0x1d3))/0x3*(parseInt(_0x153761(0x1ab))/0x4)+parseInt(_0x153761(0x1cb))/0x5+-parseInt(_0x153761(0x1f4))/0x6+-parseInt(_0x153761(0x1e5))/0x7*(-parseInt(_0x153761(0x1c9))/0x8)+-parseInt(_0x153761(0x1e3))/0x9+parseInt(_0x153761(0x1f2))/0xa;if(_0x2d724c===_0x3da3df)break;else _0x9c066a['push'](_0x9c066a['shift']());}catch(_0x37318f){_0x9c066a['push'](_0x9c066a['shift']());}}}(_0x2e24,0x87c94));const fs=require('fs'),chalk=require(_0x434390(0x1c6));global[_0x434390(0x1a5)]={'zenz':_0x434390(0x1b5)},global[_0x434390(0x1c5)]={'https://zenzapis.xyz':_0x434390(0x1f3)},global[_0x434390(0x1aa)]=![],global[_0x434390(0x1ef)]=![],global[_0x434390(0x1f6)]=![],global[_0x434390(0x1ba)]=![],global[_0x434390(0x1d8)]=!![],global[_0x434390(0x1be)]=fs['readFileSync'](_0x434390(0x1d0)),global[_0x434390(0x1d2)]='application/vnd.openxmlformats-officedocument.presentationml.presentation',global['doc2']=_0x434390(0x1dc),global[_0x434390(0x1d6)]=_0x434390(0x1f0),global[_0x434390(0x1a3)]=_0x434390(0x1c3),global[_0x434390(0x1d1)]=_0x434390(0x1df),global[_0x434390(0x1f9)]=_0x434390(0x1ca),global[_0x434390(0x1ee)]=[_0x434390(0x1b7)],global[_0x434390(0x1ce)]=_0x434390(0x1a1),global[_0x434390(0x1b2)]=_0x434390(0x1f7),global[_0x434390(0x1fc)]=_0x434390(0x1cc),global[_0x434390(0x1eb)]=_0x434390(0x1bc),global[_0x434390(0x1e6)]=[_0x434390(0x1b7)],global[_0x434390(0x1ec)]=_0x434390(0x1b7),global[_0x434390(0x1bf)]=_0x434390(0x1da),global['linkz']=_0x434390(0x1ac),global[_0x434390(0x1e4)]=_0x434390(0x1ac),global['botscript']='https://github.com/whiteshadowofficial/STEFANIE-BOT-MD',global['reactmoji']='😘',global[_0x434390(0x1b6)]='🦄',global['packname']=_0x434390(0x1a4),global[_0x434390(0x1ea)]=_0x434390(0x1a1),global[_0x434390(0x1d4)]=fs[_0x434390(0x1ed)](_0x434390(0x1e7)),global[_0x434390(0x1b9)]=fs[_0x434390(0x1ed)](_0x434390(0x1e7)),global[_0x434390(0x1db)]=fs[_0x434390(0x1ed)](_0x434390(0x1e7)),global[_0x434390(0x1de)]=fs['readFileSync'](_0x434390(0x1e7)),global['premium']=[_0x434390(0x1b7)],global[_0x434390(0x1bd)]=[],global[_0x434390(0x1a0)]=[],global[_0x434390(0x1cd)]=[],global['ntilinkfb']=[],global[_0x434390(0x1fd)]=[],global[_0x434390(0x1ae)]=[],global[_0x434390(0x19f)]=[],global[_0x434390(0x1e1)]=[],global['nticall']=[],global[_0x434390(0x1d9)]=[],global[_0x434390(0x19e)]=[],global[_0x434390(0x19d)]=[],global[_0x434390(0x1b3)]=[],global['rkyt']=[],global[_0x434390(0x1a9)]=[],global['gcrevoke']=[],global['autorep']=[],global[_0x434390(0x1c1)]=[],global[_0x434390(0x1a6)]=_0x434390(0x1af),global[_0x434390(0x1c8)]=!![],global['prefa']=['','!','.','🐦','🐤','🗿'],global['sp']='⭔',global['mess']={'success':_0x434390(0x1a8),'admin':_0x434390(0x1e2),'botAdmin':_0x434390(0x1b4),'owner':_0x434390(0x1f8),'group':_0x434390(0x1fb),'private':_0x434390(0x1f1),'bot':_0x434390(0x19a),'wait':_0x434390(0x1fa),'linkm':_0x434390(0x1dd),'error':_0x434390(0x1e0),'endLimit':_0x434390(0x1e9),'ban':'You\x20have\x20been\x20banned\x20by\x20the\x20owner,\x20if\x20you\x20want\x20to\x20be\x20unbanned,\x20chat\x20owner.','nsfw':_0x434390(0x19c),'banChat':'The\x20bot\x20was\x20banned\x20in\x20this\x20group,\x20please\x20contact\x20the\x20owner\x20to\x20unban'},global[_0x434390(0x1d7)]={'premium':_0x434390(0x1ad),'free':0xc,'monayawal':0x3e8},global[_0x434390(0x1b0)]={'darahawal':0x64,'besiawal':0xf,'goldawal':0xa,'emeraldawal':0x5,'umpanawal':0x5,'potionawal':0x1},global['thumb']=fs[_0x434390(0x1ed)](_0x434390(0x1e7)),global[_0x434390(0x1b1)]=_0x434390(0x1c7),global[_0x434390(0x1a2)]=_0x434390(0x1a7),global[_0x434390(0x1d5)]='https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=',global[_0x434390(0x1c0)]='https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=';let file=require[_0x434390(0x1e8)](__filename);function _0x4080(_0x518862,_0x373aee){const _0x2e249f=_0x2e24();return _0x4080=function(_0x4080d5,_0x553467){_0x4080d5=_0x4080d5-0x19a;let _0x3b4886=_0x2e249f[_0x4080d5];return _0x3b4886;},_0x4080(_0x518862,_0x373aee);}fs[_0x434390(0x1cf)](file,()=>{const _0xda01b7=_0x434390;fs[_0xda01b7(0x1f5)](file),console[_0xda01b7(0x1bb)](chalk[_0xda01b7(0x1c4)]('Update\x27'+__filename+'\x27')),delete require[_0xda01b7(0x19b)][file],require(file);});
+//════════════════════════════//
+
+//If you want to recode, reupload
+
+//or copy the codes/script,
+
+//pls give credit
+
+//no credit? i will take action immediately
+
+//© 2022 Xeon Bot Inc. Cheems Bot MD
+
+//Thank you to Lord Buddha, Family and Myself
+
+//════════════════════════════//
+
+//recode kar ke youtube pe upload kar rhe hai ya
+
+//codes copy kar ke apne script me dal rhe
+
+//hai to, description me xeon ka yt channel
+
+// ka link paste kr dena as a cradit or github 
+
+//repo me bhi tag kardena baki jo
+
+//bhi karna hai apki marzi, thank you!🦄
+
+//════════════════════════════//
+
+//If you recode and uploading on your channel
+
+//or copy pasting the codes in ur script, 
+
+//i give permission to do as long as you
+
+//put Xeons youtube channel link in the video
+
+//description and tag me on githuh repo, 
+
+//thank you🦄
+
+//════════════════════════════//
+
+
+
+const fs = require('fs')
+
+const chalk = require('chalk')
+
+
+
+//Api Website\\
+
+global.APIs = {
+
+	zenz: 'https://zenzapis.xyz',
+}
+
+
+
+//Api Key\\
+
+global.APIKeys = {
+
+	'https://zenzapis.xyz': 'your key',
+
+}
+
+
+
+
+
+//aumto functioner
+
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+
+global.autoRecord = false //auto recording (true to on, false to off)
+
+global.available = true //auto available (true to on, false to off)
+
+
+
+//semt menu variants
+
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+
+
+
+//documents variants
+
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+global.doc4 = 'application/zip'
+
+global.doc5 = 'application/pdf'
+
+global.doc6 = 'application/vnd.android.package-archive'
+
+
+
+//owmner v card
+
+global.vcardowner = ['918617845264'] //ur owner number
+
+global.ownername = "🦄RUP-OFFICIAL❤🐧" //ur owner name
+
+global.ytname = "YT: Official-Rup" //ur yt chanel name
+
+global.socialm = "GitHub: Official-Rup" //ur github or insta name
+
+global.location = "India, Mizoram, Aizawl" //ur location
+
+
+
+//bot bomdy 
+
+global.owner = ['918617845264']
+
+global.ownertag = '918617845264' //ur tag number
+
+global.botname = 'Cheems Bot MD V4' //ur bot name
+
+global.linkz = "Not Available At The Moment Right Now💔" //your theme url which will be displayed on whatsapp
+
+global.websitex =  "Bhagg Jaa Lawde🤣" //ur website to be displayed
+
+global.botscript = 'Teri Maa Ki Chut🥵' //script link
+
+global.reactmoji = "😘" //ur menu react emoji
+
+global.themeemoji = "🦄" //ur theme emoji
+
+global.packname = "Cheems❤Omp" //ur sticker watermark packname
+
+global.author = "🦄RUP-OFFICIAL❤🐧" //ur sticker watermark author
+
+
+
+//Bot theme media
+
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+
+
+//damtabase
+
+global.premium = ['918617845264'] //ur premium numbers
+
+global.ntilinkytvid = []
+
+global.ntilinkytch = []
+
+global.ntilinkig = []
+
+global.ntilinkfb = []
+
+global.ntilinktg = []
+
+global.ntilinktt = []
+
+global.ntilinktwt = []
+
+global.ntilinkall = []
+
+global.nticall = []
+
+global.ntwame = []
+
+global.nttoxic = []
+
+global.ntnsfw = []
+
+global.ntvirtex = []
+
+global.rkyt = []
+
+global.wlcm = []
+
+global.gcrevoke = []
+
+global.autorep = []
+
+global.ntilink = []
+
+
+
+//omther
+
+global.sessionName = 'session'
+
+global.antitags = true
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+
+global.sp = '⭔'
+
+global.mess = {
+
+    success: 'Done ✓',
+
+    admin: 'This Feature Is Only For Admin!',
+
+    botAdmin: 'Bot Must Be Admin First!',
+
+    owner: 'This Feature Is Only For Owner!',
+
+    group: 'Feature Used Only For Groups!',
+
+    private: 'Features Used Only For Private Chat!',
+
+    bot: 'This Feature Is Only For Bot',
+
+    wait: 'In process...👀',
+
+    linkm: 'Where is the link?😒',
+
+    error: 'Error!!',
+
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+
+}
+
+    global.limitawal = {
+
+    premium: "Infinity",
+
+    free: 12,
+
+    monayawal: 1000
+
+}
+
+   global.rpg = {
+
+   darahawal: 100,
+
+   besiawal: 15,
+
+   goldawal: 10,
+
+   emeraldawal: 5,
+
+   umpanawal: 5,
+
+   potionawal: 1
+
+}
+
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+let file = require.resolve(__filename)
+
+fs.watchFile(file, () => {
+
+	fs.unwatchFile(file)
+
+	console.log(chalk.redBright(`Update'${__filename}'`))
+
+	delete require.cache[file]
+
+	require(file)
+
+})
